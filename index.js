27b99f1b87120973d15d9d00f54f5f085f69f485
collince korir <77958579+collince20@users.noreply.github.com>
2022-07-19 11:24:20 +0300

    changes made

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const form = document.getElementById('form');
 const ID = document.getElementById('id');
 const Password = document.getElementById('password');
+const email = document.getElementById('email');
+const SPassword = document.getElementById('Spassword');
+const SPassword2 = document.getElementById('Spassword2');
+
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
@@ -11,6 +15,12 @@ form.addEventListener('submit',(e)=>{
 function checkInputs(){
     const IDValue = ID.Value.trim();
     const PasswordValue = Password.value.trim();
+    const emailValue = ID.Value.trim();
+    const SPasswordValue = ID.Value.trim();
+    const SPassword2Value = ID.Value.trim();
+
+
+
 
     if (ID.value === '') {
         setErrorFor(ID,'Enter ID');
